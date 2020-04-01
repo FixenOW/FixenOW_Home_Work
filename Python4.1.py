5fd2a8ae11b70c747af18ad3763f438729d22608
Mukha Vitalii <heartstone.2001@mail.ru>
2020-04-01 20:12:48 +0300

    Home Work

--- Python4.1.py
+++
@@ -1,8 +0,0 @@
-distance_to_pump=50
-mpg=2
-fuel_left=25
-def zero_fuel(distance_to_pump,mpg,fuel_left):
-    if (fuel_left*mpg>=distance_to_pump):
-        return True
-    else:
-        return False
